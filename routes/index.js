1e0d145563d5ff2423194bd0531ac8756a91afc3
Kakumba Gabriel <kakumba19@gmail.com>
2026-09-19 00:14:12 +0300

    Delete routes directory

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const qrRoute = require('./qr');
-const pairRoute = require('./pair');
-const sessionIdRoute = require('./session-id');
-
-module.exports = {
-    qrRoute,
-    pairRoute,
-    sessionIdRoute
-};
